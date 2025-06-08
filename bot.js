dfbd26ac25f989645096a63e8b39bf245f9cba28
BernardoHorta <bernardo.o.horta@gmail.com>
2025-06-08 17:36:04 -0300

    Version 1.0.2

--- bot.js
+++ bot.js
@@ -1,22 +1,13 @@
 const fs = await import('fs');
 
 //Important variables
-const admPath = '../adm.json';
+const dataPath = '../data.json';
 const dotenvPath = '../.env';
-const error_msg = 'Po, isso dai ta certo? Manda denovo. !help para qualquer duvida';
-const commands = {
-    '!help' : 'Exibe todos os comandos.',
-    '!roll (Num1) (Num2)' : 'Retorna um número aleatório entre os dois números informados.',
-    '!primes (Num1)' : 'Retorna todos os números primos de 1 até o número informado.',
-    '!primes (Num1) (Num2)' : 'Retorna todos os números primos entres os dois números informados.'
-}
-const adm_commands = {
-    '!adm add (mention)' : 'Adiciona um usuário a lista de operadores.',
-    '!adm remove (mention)' : 'Remove um usuário da lista de operadores'
-}
-
+const error_msg = 'Alem de primata, é burro. Manda um !help e ve se aprende, inútil';
+const commands = fs.readFileSync('./commands.txt', 'utf-8');
 
 // =========================== .env Configuration ===================================
+
 const dotenv = await import('dotenv')
 dotenv.config({ path : dotenvPath})  // !!! Change path to the .env archive location !!!4
 
@@ -40,7 +31,7 @@ client.on('messageCreate', async message => {
     if (message.author.bot) return;
 
     if (message.content.startsWith('!help')) {
-        await message.reply(await help());
+        await message.reply(commands);
     }
 
     if (message.content.startsWith('!roll')) {
@@ -62,6 +53,46 @@ client.on('messageCreate', async message => {
             await message.reply(await removeAdm(message.author, message.mentions.users.first()));
         }
     }
+
+    if (message.content.startsWith('!limit')) {
+        if (isAdm(message.author)) {
+            if (message.member.voice.channel) {
+                let limit = message.content.split(' ')[1]; //!Limit <@channelID> *limit*
+                limit = parseInt(limit);
+                if (!isNaN(limit)) {
+                    if (limit > 99 || limit < 0) return await message.reply('Entre 1 e 99 colega. (0) p/ sem limite.');
+                    await message.member.voice.channel.setUserLimit(limit);
+                    if (limit === 0) {
+                        return await message.reply(`${message.member.voice.channel} foi definido para sem limite de usuarios.`);
+                    }else {
+                        return await message.reply(`${message.member.voice.channel} foi definido para no maximo ${limit} usuários.`);
+                    }
+                }else {
+                    return await message.reply(error_msg);
+                }
+            }else {
+                return await message.reply('Entre em uma call primeiro amigo(a).');
+            }
+        }
+        return await message.reply('Comando somente para operadores.');
+    }
+
+    if (message.content.startsWith('!kick')) {
+        const mentions = message.mentions.users;
+        if (isAdm(message.author)) {
+            if(mentions.size > 0) {
+                const user = await message.guild.members.fetch(mentions.first().id);
+                if (isAdm(user, true)) {return await message.reply('Nao tem como kickar esse cara. Ele e absoluto.')}
+                if (user.voice.channel) {
+                    await user.voice.disconnect()
+                    return await message.reply('Usuário desconectado com sucesso.')
+                }
+            }
+            return await message.reply(error_msg);
+        }
+        return await message.reply('Voce nao tem autoridade!');
+    }
+
 });
 
 client.once('ready', () => {
@@ -72,30 +103,30 @@ await client.login(process.env.DISCORD_TOKEN);
 
 // ================================== Funcionalidades ===================================
 
-async function help () {
+function isAdm(user, isSudo=false) {
+    const data = JSON.parse(fs.readFileSync(dataPath, 'utf-8'));
+    const sudo_users = process.env.SUDO_USERS;
 
-    // Object to text.
-    let reply = '**Comandos gerais:** \n\n';
-    for (let i=0; i < Object.keys(commands).length; i++) {
-        reply += `+ **${Object.keys(commands)[i]}** : ${Object.values(commands)[i]}\n`;
+    if (isSudo) {
+        return (sudo_users.includes(user.id));
+    }else {
+        return (data.adm.includes(user.id) || sudo_users.includes(user.id));
     }
-    reply += '\n**Comandos de operadores:**\n\n';
-    for (let i=0; i < Object.keys(adm_commands).length; i++) {
-        reply += `+ **${Object.keys(adm_commands)[i]}** : ${Object.values(adm_commands)[i]}\n`;
-    }
-    return reply;
 }
 
 async function addAdm(req_user, user) {
-    let adm = JSON.parse(fs.readFileSync(admPath, 'utf-8'));
+    let data = JSON.parse(fs.readFileSync(dataPath, 'utf-8'));
+    let adm = data.adm;
     const sudo_users = process.env.SUDO_USERS;
 
-    if (!adm.includes(req_user.id) && !sudo_users.includes(req_user.id)) return 'Amigao, voce nem permissao tem e acha que pode pedir para dar permissao a outro?.';
+    if (!adm.includes(req_user.id) && !sudo_users.includes(req_user.id)) return 'Amigo, voce nem permissão tem e acha que pode pedir para dar permissão a outro?.';
     if (sudo_users.includes(user.id)) return `${user}  já pertence a uma categoria superior.`;
 
     if (!adm.includes(user.id)) {
         adm.push(user.id);
-        fs.writeFileSync(admPath, JSON.stringify(adm));
+
+        data.adm = adm;
+        fs.writeFileSync(dataPath, JSON.stringify(data));
         return `Usuário adicionado a lista de operadores com sucesso`;
     } else {
         return 'Usuário ja adicionado anteriormente. Qualquer duvida, digite !help.'
@@ -103,15 +134,18 @@ async function addAdm(req_user, user) {
 }
 
 async function removeAdm(req_user, user) {
-    let adm = JSON.parse(fs.readFileSync(admPath, 'utf-8'));
+    let data = JSON.parse(fs.readFileSync(dataPath, 'utf-8'));
+    let adm = data.adm;
     const sudo_users = process.env.SUDO_USERS;
 
-    if (!adm.includes(req_user.id) && !sudo_users.includes(req_user.id)) return 'Amigao, voce nem permissao tem e acha que pode pedir para dar permissao a outro?.';
+    if (!adm.includes(req_user.id) && !sudo_users.includes(req_user.id)) return 'Amigo, voce nem permissão tem e acha que pode pedir para dar permissão a outro?.';
     if (sudo_users.includes(user.id)) return `${user} não pode ser removido, pois pertence a uma categoria superior.`;
 
     if (adm.includes(user.id)) {
-        adm.filter(id => id !== user.id);
-        fs.writeFileSync(admPath, JSON.stringify(adm));
+        adm = adm.filter(id => id !== user.id);
+
+        data.adm = adm;
+        fs.writeFileSync(dataPath, JSON.stringify(data));
         return `Usuário removido com sucesso`;
     } else {
         return 'Usuário nao esta na lista de operadores . Qualquer duvida, digite !help.'
